9749510481abf0f1ca5c588f711728bb56a01398
DESKTOP-LHRQ2R4\adamsives <adamsives@gmail.com>
2018-04-22 11:10:33 +1000

    Lecture 41 - Rigid rocket

---
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        print("started!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("su-be-su plessed");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("lotating reft plessed");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("lotating light plessed");
+        }
+
+    }
+}
