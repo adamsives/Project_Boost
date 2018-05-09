7921ab80b0703264d90189726177b7b6a26b1ed9
DESKTOP-LHRQ2R4\adamsives <adamsives@gmail.com>
2018-05-09 18:11:17 +1000

    Lecture 61 working copy

---
+++ 3_Project_Boost/Assets/Oscillator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+
+public class Oscillator : MonoBehaviour {
+
+    [SerializeField] private Vector3 movementVector = new Vector3(10.0f, 10.0f, 10.0f);
+    
+    [Range(0,1)] [SerializeField] float movementFactor;
+
+    [SerializeField] float period = 2f;
+    
+    Vector3 startPos;
+
+    // Use this for initialization
+    void Start () {
+        startPos = transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float cycle = Time.time /period;
+
+        const float TAU = Mathf.PI * 2f;
+
+        float rawSinWave = Mathf.Sin(cycle * TAU);
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+
+        Vector3 offset = movementFactor * movementVector;
+
+        transform.position = startPos + offset;
+    }
+}
--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -1,18 +1,28 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
-
     Rigidbody rigidbody;
     Transform transform;
     [SerializeField] float rcsThrust = 250f;
     [SerializeField] float rcsMainThrust = 50f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip deathExplosion;
+    [SerializeField] AudioClip landingSuccess;
 
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem deathExplosionParticles;
+    [SerializeField] ParticleSystem landingSuccessParticles;
+
+    enum State { Alive, Dying, Transcending}
+
+    [SerializeField] State state = State.Alive;
+    
     AudioSource audio;
+
     // Use this for initialization
+
     void Start()
     {
         //print("started!");
@@ -24,25 +34,60 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Rotate();
-        Thrust();
+        //todo stop sound on death
+        if (state == State.Alive){
+            Rotate();
+            Thrust();
+        }
+
     }
 
-    void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
+
+        if (state != State.Alive) {return;}
+
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-
-                print("collided");
                 break;
-            case "Fuel":
-                print("Fuel");
+            case "Finish":
+                StartSuccessSequence();
                 break;
             default:
-                print("DEAD!");
+                StartDeathSequence();
                 break;
         }
+    }
+
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        audio.Stop();
+        audio.PlayOneShot(landingSuccess);
+        landingSuccessParticles.Play();
+        Invoke("LoadNextLevel", 1f);
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audio.Stop();
+        audio.PlayOneShot(deathExplosion);
+        deathExplosionParticles.Play();
+        Invoke("LoadFirstLevel", 1f);
+        rigidbody.AddRelativeForce(Vector3.back);
+    }
+
+    private void LoadFirstLevel()
+    {
+        print("LoadFirstLevel");
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
     }
 
     private void Rotate()
@@ -69,16 +114,25 @@ public class Rocket : MonoBehaviour
 
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidbody.AddRelativeForce(Vector3.up * forwardThrustThisFrame);
-            if (!audio.isPlaying)
-            {
-                audio.Play();
-            }
+            ApplyThrust(forwardThrustThisFrame);
         }
         else
         {
             audio.Stop();
+            mainEngineParticles.Stop();
         }
         rigidbody.freezeRotation = false;
+    }
+
+    private void ApplyThrust(float forwardThrustThisFrame)
+    {
+        rigidbody.AddRelativeForce(Vector3.up * forwardThrustThisFrame * Time.deltaTime);
+        if (!audio.isPlaying)
+        {
+            audio.PlayOneShot(mainEngine);
+        }
+
+        mainEngineParticles.Play();
+
     }
 }
